35b9234af754206fb11e565935a77fa1a2c69ee0
belek0 <lipendina.tn@gmail.com>
2019-06-18 17:38:19 +0400

    Добавлена задача 2.3.4.Принимаем alert

---
+++ Chapter 2/2_3_4.py
@@ -0,0 +1,32 @@
+# Открыть страницу http://suninjuly.github.io/alert_accept.html
+# Нажать на кнопку
+# Принять confirm
+# На новой странице решить капчу для роботов, чтобы получить число с ответом
+
+from selenium import webdriver
+import math
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
+
+confirm = browser.switch_to.alert
+confirm.accept()
+
+x_element = browser.find_element_by_id("input_value")
+x = x_element.text
+y = calc(x)
+
+input1 = browser.find_element_by_id("answer")
+input1.send_keys(y)
+
+btn = browser.find_element_by_css_selector("button.btn")
+btn.click()
+
+
